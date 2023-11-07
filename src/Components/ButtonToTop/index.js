b7a7390e1d3282704c02568423a695f1c1534c8e
João de Menezes <juniormenezes384@gmail.com>
2023-11-06 22:34:02 -0300

    Criação componente ButtonTop

---
+++ src/Components/ButtonToTop/index.js
@@ -0,0 +1,40 @@
+//Botão que move a tela para o topo da página
+
+import React, { useState, useEffect } from 'react';
+import styles from './Button.module.scss';
+
+import SetaTopo from '../../Assets/SetaTopo.png';
+
+const ButtonToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <button
+      className={`${styles.buttonToTop} ${isVisible ? styles.visible : ''}`}
+      onClick={scrollToTop}
+    >
+      <img src={SetaTopo} alt="Seta topo" className={styles.buttonImage} />
+    </button>
+  );
+};
+
+export default ButtonToTop;
