76e177d90de15acaf57aa4479641f6a238172f04
João de Menezes <juniormenezes384@gmail.com>
2023-11-10 21:27:40 -0300

    test: fazend testes no index principal

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { ChakraProvider } from '@chakra-ui/react'
-import pacienteA from './Pages/fichaPacientes/pacienteA'
 import ButtonToTop from './Components/ButtomToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-    {pacienteA()}
-    </ChakraProvider>
+    <div> Hellor World</div>
     <ButtonToTop/>
   </React.StrictMode>
 );
