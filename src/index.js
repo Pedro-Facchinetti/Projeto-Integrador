56605dc5527e2e32072a1ce6852bf10cacd1fef7
João de Menezes <juniormenezes384@gmail.com>
2023-11-10 21:16:30 -0300

    Revert "Merge pull request #2 from Kcpssz/main"
    This reverts commit f23b5795f928e9f8dbbc2d441c4f3852d944d845, reversing
    changes made to 3445d435f51103606e2f217830ac222d41177344.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 
-import Anamnese from './Pages/Anamnese';
-import ButtonToTop from './Components/ButtomToTop';
-=======
-//import './css/global.scss';//
 import { ChakraProvider } from '@chakra-ui/react'
 import pacienteA from './Pages/fichaPacientes/pacienteA'
->>>>>>> f23b5795f928e9f8dbbc2d441c4f3852d944d845
+import ButtonToTop from './Components/ButtomToTop';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Anamnese/>
-    <ButtonToTop/>
-  </React.StrictMode>
-);
     <ChakraProvider>
     {pacienteA()}
     </ChakraProvider>
-  </React.StrictMode>,
-)
-
+    <ButtonToTop/>
+  </React.StrictMode>
+);
