4f215113813ec1852db9823305a023b60639f75e
João de Menezes <juniormenezes384@gmail.com>
2023-11-14 10:36:33 -0300

    add: index principal arrumado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 import ButtonToTop from './Components/ButtomToTop';
+import Anamnese from './Pages/AnamnesePaciente/Anamnese';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div> Hellor World</div>
-    <ButtonToTop/>
+    <ChakraProvider>
+      {Anamnese()}
+    </ChakraProvider>
+    <ButtonToTop />
   </React.StrictMode>
 );
