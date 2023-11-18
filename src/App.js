c92c614d7b1d317b072fce41a6ee4eef6fcc6907
João de Menezes <juniormenezes384@gmail.com>
2023-11-18 14:52:27 -0300

    Add: barra de navegação e App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Route, Routes } from "react-router-dom";
+
+import ButtonToTop from './Components/ButtomToTop/index';
+
+import Anamnesse from "./Pages/AnamnesePaciente/Anamnese"
+import Assinatura from "./Pages/Assinatura/Assinatura"
+import PacienteA from "./Pages/fichaPacientes/PacienteA"
+import PacienteInf from "./Pages/fichaPacientes/PacienteInf"
+import Orcamento from "./Pages/Orçamento/Orcamento"
+
+function App() {
+    return (
+        <ChakraProvider>
+            <div>
+                <Routes>
+                    <Route path='/anamnese' element={<Anamnesse />} />
+                    <Route path='/assinatura' element={<Assinatura />} />
+                    <Route path='/fichaPaciente/paciente_adulto' element={<PacienteA />} />
+                    <Route path='/fichaPaciente/paciente_infantil' element={<PacienteInf />} />
+                    <Route path='/orcamento' element={<Orcamento />} />
+                </Routes>
+            </div>
+            <ButtonToTop />
+        </ChakraProvider>
+    );
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,44 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function NavBar() {
+    const location = useLocation();
+
+    return (
+        <nav className="nav" style={{ background: '#24394A', borderRadius: '10px', padding: '20px', overflow: 'hidden', width: '100%' }}>
+            <ul style={{ display: 'flex', listStyle: 'none', padding: 0, margin: 0, justifyContent: 'center', gap: '35px' }}>
+                <CustomLink to={"/anamnese"} isActive={location.pathname === '/anamnese'}>Anamnese</CustomLink>
+                <CustomLink to={"/assinatura"} isActive={location.pathname === '/assinatura'}>Assinatura</CustomLink>
+                <CustomLink to={"/fichaPaciente/paciente_adulto"} isActive={location.pathname === '/fichaPaciente/paciente_adulto'}>Fixa Clínica</CustomLink>
+                <CustomLink to={"/orcamento"} isActive={location.pathname === '/orcamento'}>Orçamento</CustomLink>
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink({ to, isActive, children, ...props }) {
+    return (
+        <li>
+            <Link
+                to={to}
+                style={{
+                    display: 'flex',
+                    padding: '10px 20px',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '10px',
+                    borderRadius: '10px',
+                    background: isActive ? '#13232F' : '#13232F',
+                    color: 'white',
+                    textDecoration: 'none',
+                    border: isActive ? '1px solid #11C8B4' : 'none',
+                    boxShadow: isActive ? '0px 4px 4px 0px rgba(190, 190, 190, 0.25)' : 'none',
+                    fontSize: '16px', // Tamanho da fonte fixo
+                    width: '170px',  // Largura fixa
+                }}
+                {...props}
+            >
+                {children}
+            </Link>
+        </li>
+    )
+}
